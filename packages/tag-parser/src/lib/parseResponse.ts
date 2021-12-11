4d6bc883b1af90e872cab4dd531401f1b7ed4f83
UndiedGamer <hemanthchakka2007@gmail.com>
2021-12-11 19:27:08 +0530

    feat: need to do some tests later doe

--- packages/tag-parser/src/lib/parseResponse.ts
+++ packages/tag-parser/src/lib/parseResponse.ts
@@ -1,16 +1,11 @@
 import { parsingRegExp } from "./parseData";
 
-export async function parseResponse(response: string, interaction: any) {
+export function parseResponse(response: string, interaction: any) {
 	if (!parsingRegExp.test(response)) return interaction.reply(response)
-	const parsedResponse = response.match(parsingRegExp)!;
-	switch (parsedResponse[0].replace('{{', '').replace('}}', '')) {
-		case 'member':
-			return response.replace('{{member}}', interaction.member);
-		case 'user':
-			return response.replace('{{user}}', interaction.member.user);
-		case 'channel':
-			return response.replace('{{channel}}', interaction.channel);
-		case 'guild':
-			return response.replace('{{guild}}', interaction.channel.guild);
-	}
+	response = response.replaceAll('}}', '')
+	response = response.replaceAll('{{member', interaction.member)
+	response = response.replaceAll('{{user', interaction.member.user)
+	response = response.replaceAll('{{channel', interaction.channel)
+	response = response.replaceAll('{{guild', interaction.guild)
+	return interaction.reply(response)
 }
