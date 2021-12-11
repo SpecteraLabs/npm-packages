a08d0ad18f92ddf6d940db40ef6582c10977748d
UndiedGamer <hemanthchakka2007@gmail.com>
2021-12-11 18:37:34 +0530

    chore: better scripts

--- packages/tag-parser/scripts/clean.mjs
+++ packages/tag-parser/scripts/clean.mjs
@@ -3,5 +3,7 @@ import { URL } from 'url';
 
 const rootFolder = new URL('../', import.meta.url);
 const distFolder = new URL('dist/', rootFolder);
+const bundleFolder = new URL('bundled/', rootFolder);
 
 await rm(distFolder, { recursive: true, force: true });
+await rm(bundleFolder, { recursive: true, force: true });
