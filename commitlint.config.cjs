eb42cb77b8baecdec66c990036ae095243715518
UndiedGamer <hemanthchakka2007@gmail.com>
2022-03-18 15:57:43 +0530

    feat(brawlstats)!: remove logger

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,15 +1,6 @@
 module.exports = {
-	extends: ['@commitlint/config-angular', '@commitlint/parse'],
-	rules: {
-		'scope-case': [2, 'always', 'pascal-case'],
-		'type-enum': [2, 'always', ['chore', 'build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']]
-	},
-	parserPreset: {
-		parserOpts: {
-			headerPattern: /^(\w*)!?(?:\((.*)\))?: (.*)$/,
-			headerCorrespondence: ['type', 'scope', 'subject'],
-			revertPattern: /^(?:Revert|revert:)\s"?([\s\S]+?)"?\s*This reverts commit (\w*)\./i,
-			revertCorrespondence: ['header', 'hash']
-		}
-	}
+  extends: [
+    "@commitlint/parse",
+    "@commitlint/config-lerna-scopes",
+  ],
 };
