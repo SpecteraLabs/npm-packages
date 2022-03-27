8c10a3efe472088562f1e7bd9f4f097681e21bca
UndiedGamer <hemanthchakka2007@gmail.com>
2022-03-27 15:16:11 +0530

    chore: update code

--- packages/brawl-api/src/app.module.ts
+++ packages/brawl-api/src/app.module.ts
@@ -1,13 +1,10 @@
-import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BrawlersController } from './brawlers/brawlers.controller';
-import { BrawlersService } from './brawlers/brawlers.service';
 
 @Module({
-  imports: [HttpModule],
   controllers: [AppController, BrawlersController],
-  providers: [AppService, BrawlersService],
+  providers: [AppService],
 })
 export class AppModule {}
