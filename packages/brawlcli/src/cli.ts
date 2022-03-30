7557fc0438868b4551b6381c4a1c52ad0297f0d5
UndiedGamer <hemanthchakka2007@gmail.com>
2022-03-30 18:37:53 +0530

    chore: init brawlcli

---
+++ packages/brawlcli/src/cli.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+import { readFile } from "node:fs/promises";
+import { URL } from 'node:url'
+
+const brawlstats = new Command();
+
+const packageJSON = new URL("../package.json", import.meta.url);
+const pkg = JSON.parse(await readFile(packageJSON, "utf-8"));
+
+brawlstats //
+	.name('brawlstats')
+  	.version(pkg.version)
+
+// TODO
