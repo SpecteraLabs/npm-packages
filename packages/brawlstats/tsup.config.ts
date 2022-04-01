910b4619d907b1381ca35ab964f2d933c51431da
UndiedGamer <hemanthchakka2007@gmail.com>
2022-04-01 18:52:45 +0530

    feat: add support for iife bundles

--- packages/brawlstats/tsup.config.ts
+++ packages/brawlstats/tsup.config.ts
@@ -4,13 +4,14 @@ export default defineConfig({
 	clean: true,
 	dts: false,
 	entry: ['src/index.ts'],
-	format: ['esm', 'cjs'],
+	format: ['esm', 'cjs', 'iife'],
 	minify: false,
 	skipNodeModulesBundle: true,
 	sourcemap: true,
 	target: 'es2021',
 	tsconfig: 'src/tsconfig.json',
 	keepNames: true,
+	globalName: 'BrawlStats',
 	esbuildOptions: (options, context) => {
 		if (context.format === 'cjs') {
 			options.banner = {
