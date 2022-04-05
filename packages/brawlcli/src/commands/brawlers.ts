3fc116ad2c9c0362466106ae97aacd0cb6efa245
UndiedGamer <hemanthchakka2007@gmail.com>
2022-04-05 10:02:10 +0530

    feat: complete brawlers command

--- packages/brawlcli/src/commands/brawlers.ts
+++ packages/brawlcli/src/commands/brawlers.ts
@@ -9,7 +9,21 @@ export async function getBrawler(name?: string) {
 			token: config.token
 		});
 		const brawlers = await client.getBrawlers();
-		if (!name) {
+		if (name) {
+			const brawler = brawlers.find((brawler) => brawler.name === name.toUpperCase());
+			if (brawler) {
+				console.table([
+					{
+						name: brawler.name,
+						id: brawler.id,
+						'Star Powers': brawler.starPowers?.map((star) => star.name).join(', '),
+						Gadgets: brawler.gadgets?.map((gadget) => gadget.name).join(', ')
+					}
+				]);
+			} else {
+				console.error(`Could not find brawler with name ${name}`);
+			}
+		} else {
 			const mapped = brawlers.map((brawler) => {
 				return {
 					name: brawler.name,
