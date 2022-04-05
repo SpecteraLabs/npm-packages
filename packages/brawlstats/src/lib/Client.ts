de26ab5d1d0a014fe7a5125b74ea1f59a75511c9
UndiedGamer <hemanthchakka2007@gmail.com>
2022-04-05 18:42:26 +0530

    feat: add managers inside Client
    BREAKING CHANGE: the methods Client#get* are removed now

--- packages/brawlstats/src/lib/Client.ts
+++ packages/brawlstats/src/lib/Client.ts
@@ -1,56 +1,19 @@
-import { BrawlersMap } from './Brawlers';
-import { Club } from './structures/Club';
-import { Event } from './structures/Events';
-import { Player } from './structures/Player';
+import { makeBrawlersMap } from './makeBrawlersMap';
+import { ClubManager } from './managers/ClubManager';
+import { PlayerManager } from './managers/PlayerManager';
 
 export class Client {
 	#token!: string;
 	public options: ClientOptions;
+	public brawlers = makeBrawlersMap();
+	public players: PlayerManager;
+	public clubs: ClubManager;
 
 	public constructor(options?: ClientOptions) {
 		this.options = options!;
 		this.#token = options?.token ?? (process.env.BRAWLSTARS_TOKEN as string);
-	}
-
-	/**
-	 * Get details of a player from the API.
-	 * @param {String} tag The tag of the player.
-	 * @async
-	 * @returns {Promise<Player>} The player from the api
-	 */
-	public getPlayer(tag: string): Promise<Player> {
-		tag = tag.replace('#', '%23');
-		return Player.getPlayer(tag, this.#token);
-	}
-
-	/**
-	 * Get a list of brawlers from the API which are sent back to you as a {@link BrawlersMap}.
-	 * @async
-	 * @returns {Promise<BrawlersMap>} The brawlers map from the api
-	 */
-	public async getBrawlers(): Promise<BrawlersMap> {
-		const res = new BrawlersMap(this.#token);
-		return res.init();
-	}
-
-	/**
-	 * Get details of a club from the API.
-	 * @async
-	 * @param {String} tag The tag of the club.
-	 * @returns {Promise<Club>} The club from the api.
-	 */
-	public async getClub(tag: string): Promise<Club> {
-		tag = tag.replace('#', '%23');
-		return Club.getClub(tag, this.#token);
-	}
-
-	/**
-	 * Get the events rotation from the API.
-	 * @async
-	 * @returns The events from the api.
-	 */
-	public async getRotation() {
-		return Event.getRotation(this.#token);
+		this.players = new PlayerManager(this.#token);
+		this.clubs = new ClubManager(this.#token);
 	}
 }
 
