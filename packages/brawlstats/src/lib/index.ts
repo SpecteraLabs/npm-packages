17188769a58397c82018e54dacb2dac2d0c952b9
UndiedGamer <hemanthchakka2007@gmail.com>
2022-04-14 16:48:37 +0530

    fix: export more types for typescript users

--- packages/brawlstats/src/lib/index.ts
+++ packages/brawlstats/src/lib/index.ts
@@ -2,5 +2,6 @@ export { Client } from './Client';
 export type { Club } from './structures/Club';
 export type { Player } from './structures/Player';
 export type { IEvent as Event } from './types';
+export type { Leaderboard } from './structures/Leaderboard';
 export { dateFromBrawlStars } from './utils';
 export * from './types/enums';
