303e0e099c6ed7f6740045515dced72321e7b618
UndiedGamer <hemanthchakka2007@gmail.com>
2022-05-07 20:37:21 +0530

    chore: deprecate tag-parser

--- packages/tag-parser/scripts/clean.mjs
+++
@@ -1,9 +0,0 @@
-import { rm } from 'node:fs/promises';
-import { URL } from 'url';
-
-const rootFolder = new URL('../', import.meta.url);
-const distFolder = new URL('.rollup.cache/', rootFolder);
-const bundleFolder = new URL('bundled/', rootFolder);
-
-await rm(distFolder, { recursive: true, force: true });
-await rm(bundleFolder, { recursive: true, force: true });
--- packages/tag-parser/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/TagParser';
-
-export * from './lib/errors/ParsingError';
