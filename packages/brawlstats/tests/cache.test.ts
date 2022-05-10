e552487189eaec9572ced9180ef5fd7b85efe63d
UndiedGamer <hemanthchakka2007@gmail.com>
2022-05-10 18:43:20 +0530

    tests: update cache test

--- packages/brawlstats/tests/cache.test.ts
+++ packages/brawlstats/tests/cache.test.ts
@@ -12,8 +12,8 @@ test('test internal caching system for players', async () => {
 	// @ts-expect-error need this for test
 	const player2 = await client.players.fetch('#22QJ0JPVJ');
 	const { duration: cacheDuration } = stopwatch;
-	// cached player is returned in nanoseconds while uncache player is returned in seconds
-	expect(cacheDuration * 1000 < duration).toBe(true);
+	// cached player is returned in nanoseconds while uncached player is returned in seconds
+	expect(Number(cacheDuration.toFixed(0)) < Number(duration.toFixed(0))).toBe(true);
 });
 
 test('test internal caching system for clubs', async () => {
@@ -26,6 +26,6 @@ test('test internal caching system for clubs', async () => {
 	// @ts-expect-error need this for test
 	const club2 = await client.clubs.fetch('#2CU9RRVL');
 	const { duration: cacheDuration } = stopwatch;
-	// cached player is returned in nanoseconds while uncache player is returned in seconds
-	expect(cacheDuration * 1000 < duration).toBe(true);
+	// cached club is returned in nanoseconds while uncached club is returned in seconds
+	expect(Number(cacheDuration.toFixed(0)) < Number(duration.toFixed(0))).toBe(true);
 });
