8a0c6252b25bd361acd1d0323d139b3856cbeaef
UndiedGamer <hemanthchakka2007@gmail.com>
2022-05-11 10:56:20 +0530

    tests: more coverage

--- packages/brawlstats/tests/cache.test.ts
+++ packages/brawlstats/tests/cache.test.ts
@@ -36,3 +36,21 @@ test('test internal caching system for clubs', async () => {
 	// cached club is returned in nanoseconds while uncached club is returned in seconds
 	expect(Number(cacheDuration.toFixed(0)) < Number(duration.toFixed(0))).toBe(true);
 });
+
+test('check if cache is cleared (coverage tells this)', async () => {
+	async function wait(ms: number) {
+		return new Promise((resolve) => setTimeout(resolve, ms));
+	}
+	const client = new Client({
+		cache: {
+			player: {
+				timeout: seconds(2)
+			}
+		}
+	});
+	// @ts-expect-error need this for test
+	const player = await client.players.fetch('#22QJ0JPVJ');
+	await wait(2010);
+	const player2 = await client.players.fetch('#22QJ0JPVJ');
+	expect(player2).toBeDefined();
+});
