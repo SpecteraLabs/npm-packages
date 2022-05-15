26ca38359f96d17ab43dd12345f0bae84e6261fb
UndiedGamer <hemanthchakka2007@gmail.com>
2022-05-15 17:28:22 +0530

    revert: "fix: issues with umd bundles"

--- packages/brawlstats/src/lib/structures/Structure.ts
+++ packages/brawlstats/src/lib/structures/Structure.ts
@@ -1,4 +1,5 @@
-import type { FetchResultTypes } from '@sapphire/fetch';
+import { fetch } from '@sapphire/fetch';
+
 export class Structure {
 	#url = 'https://bsproxy.royaleapi.dev/v1';
 	#endpoint: string;
@@ -8,24 +9,10 @@ export class Structure {
 
 	public async request<T>(endpoint = this.#endpoint, token: string = process.env.BRAWLSTARS_TOKEN as string) {
 		if (this.#endpoint !== endpoint) endpoint = `${this.#endpoint}/${endpoint}`;
-		return globalThis.safetch<T>(`${this.#url}/${endpoint}`, {
+		return fetch<T>(`${this.#url}/${endpoint}`, {
 			headers: {
 				Authorization: `Bearer ${token}`
 			}
 		});
 	}
-}
-
-declare namespace globalThis {
-	function safetch<R>(url: URL | string, type?: FetchResultTypes.JSON): Promise<R>;
-	function safetch<R>(url: URL | string, options: RequestInit, type?: FetchResultTypes.JSON): Promise<R>;
-	function safetch(url: URL | string, type: FetchResultTypes.Buffer): Promise<Buffer>;
-	function safetch(url: URL | string, options: RequestInit, type: FetchResultTypes.Buffer): Promise<Buffer>;
-	function safetch(url: URL | string, type: FetchResultTypes.Blob): Promise<Blob>;
-	function safetch(url: URL | string, options: RequestInit, type: FetchResultTypes.Blob): Promise<Blob>;
-	function safetch(url: URL | string, type: FetchResultTypes.Text): Promise<string>;
-	function safetch(url: URL | string, options: RequestInit, type: FetchResultTypes.Text): Promise<string>;
-	function safetch(url: URL | string, type: FetchResultTypes.Result): Promise<Response>;
-	function safetch(url: URL | string, options: RequestInit, type: FetchResultTypes.Result): Promise<Response>;
-	function safetch<R>(url: URL | string, options: RequestInit, type: FetchResultTypes): Promise<Response | Blob | Buffer | string | R>;
 }
--- packages/brawlstats/tsup.config.ts
+++ packages/brawlstats/tsup.config.ts
@@ -4,42 +4,18 @@ export default defineConfig({
 	clean: true,
 	dts: true,
 	entry: ['src/index.ts'],
-	format: ['esm', 'iife', 'cjs'],
+	format: ['esm', 'cjs'],
 	minify: false,
 	skipNodeModulesBundle: true,
 	sourcemap: true,
 	target: 'es2021',
 	tsconfig: 'src/tsconfig.json',
 	keepNames: true,
-	globalName: 'BrawlStats',
 	esbuildOptions: (options, context) => {
-		switch (context.format) {
-			case 'cjs': {
-				options.banner = {
-					js: [
-						//
-						'"use strict";',
-						'globalThis.safetch = require("@sapphire/fetch");'
-					].join('\n')
-				};
-				break;
-			}
-			case 'esm': {
-				options.banner = {
-					js: [
-						//
-						'import { fetch as safetch } from "@sapphire/fetch";',
-						'globalThis.safetch = safetch;'
-					].join('\n')
-				};
-				break;
-			}
-			case 'iife': {
-				options.banner = {
-					js: 'globalThis.safetch = globalThis.fetch'
-				};
-				break;
-			}
+		if (context.format === 'cjs') {
+			options.banner = {
+				js: '"use strict";'
+			};
 		}
 	}
 });
